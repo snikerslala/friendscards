38e48c266d19444d4cacd019d594ff80b9c6079d
Martyna Skup <46429607+snikerslala@users.noreply.github.com>
2019-09-08 11:15:19 +0200

    Updates

---
+++ precache-manifest.82ab226b25d808e762f6d0960d4ad4e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea49dd0aec6b14e4e4164150e90c8457",
+    "url": "/friendscards/index.html"
+  },
+  {
+    "revision": "e200a0e0c2280c9fdfe9",
+    "url": "/friendscards/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "fd3f9bb45578d4ed9fc1",
+    "url": "/friendscards/static/css/main.ad33cc54.chunk.css"
+  },
+  {
+    "revision": "e200a0e0c2280c9fdfe9",
+    "url": "/friendscards/static/js/2.ed4ccca4.chunk.js"
+  },
+  {
+    "revision": "fd3f9bb45578d4ed9fc1",
+    "url": "/friendscards/static/js/main.bb5a421d.chunk.js"
+  },
+  {
+    "revision": "a8320c93e441803a89fa",
+    "url": "/friendscards/static/js/runtime~main.51c8286e.js"
+  }
+]);
